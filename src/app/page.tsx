9fd4a32141c3cb26f2d4eca629838fb09e1abc40
Daniela Cortez <d.cortezv@duocuc.cl>
2024-03-10 21:59:26 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="pt-14 flex flex-col items-center">
         <h1 className="hidden">Spark Code</h1>
         <h2 className="text-clamp-xl font-kufam font-bold mb-12 text-white">
-          Proximamente
+          Próximamente
         </h2>
         <div className="max-w-[200px] sm:max-w-full">
           <Image
