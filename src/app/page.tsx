0cbe31f4b35fd8d7a39ba4d02f10c03e27ea1eae
Leslie Herrera - Dereemii <62083149+Dereemii@users.noreply.github.com>
2024-03-11 18:16:24 +0000

    add register buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/Button";
 import { CountdownTimer } from "@/components/CountdownTimer";
 import { getMetaData, getViewports } from "@/lib/metadata";
 import Image from "next/image";
@@ -9,20 +10,24 @@ export const generateViewport = () => getViewports();
 export default function Home() {
   return (
     <main className="container  flex flex-col mx-auto my-auto">
-      <div className="pt-14 flex flex-col items-center">
+      <div className="pt-8 flex flex-col items-center">
         <h1 className="hidden">Spark Code</h1>
-        <h2 className="text-clamp-xl font-kufam font-bold mb-12 text-white">
+        <h2 className="text-clamp-xl font-kufam font-bold mb-8 text-white">
           Próximamente
         </h2>
         <div className="max-w-[200px] sm:max-w-full">
           <Image
             src={"/images/spark-only.png"}
-            width={300}
-            height={280}
+            width={200}
+            height={100}
             alt="Logo Spark Codes"
           />
         </div>
         <CountdownTimer />
+        <h2 className="text-lg font-kufam font-bold mb-8 mt-8 text-white mt-2">
+          Regístrate para los eventos:
+        </h2>
+        <Button/>
       </div>
     </main>
   );
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+export const Button = () => {
+    return (
+        <div className="flex items-center justify-center">
+        <div className="flex gap-6">
+         <a href="https://lu.ma/sparkcode-previa" target="_blank">
+                <button  type="button" title="Registrarse para Online"  className="bg-[#6e0584] hover:bg-[#e199f5] font-bold text-white font-kufam  py-2 px-10 rounded shadow-lg shadow-gray-500/50  text-white rounded-lg">
+                Online
+                </button>
+                </a>
+                <a href="https://lu.ma/sparkcode" target="_blank">
+                    <button type="button" title="Registrarse para Presencial"  className="bg-[#e199f5] hover:bg-[#6e0584] text-[#35103c] font-bold  py-2 px-8  font-kufam rounded shadow-lg shadow-gray-500/50  text-white rounded-lg">
+                    Presencial
+                </button>
+                </a>
+        </div>
+    </div>
+    );
+  };
+  
