88bd0d9ec56b53a675d7126aedfa8e6ab88c369e
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-14 22:44:18 -0300

    refactor(landing): ♻️ optimize imports and clean up layout structure
    Refine the `Landing` component by consolidating component imports and simplifying
    the lib function imports. Also, adjust the CSS class ordering and spacing for
    better readability and maintainability.
    
    Key changes include:
    - Combine `Button` and `CountdownTimer` imports from `@/components` into a
      single line to streamline component imports.
    - Simplify lib function imports (`getMetaData` and `getViewports`) by using a
      direct import from `@/lib` instead of specifying individual files, enhancing
      the clarity and brevity of import statements.
    - Minor adjustments in CSS class application order within the JSX structure to
      maintain consistency and improve readability.
    - Merge duplicate margin (`mt-8` and `mt-2` into a single `my-8`) classes for the
      second `<h2>` element to simplify styling and ensure uniform spacing around
      elements.
    
    These modifications do not alter the functionality but significantly improve
    the codebase's organization, making it easier to read and maintain.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,24 @@
-import { Button } from "@/components/Button";
-import { CountdownTimer } from "@/components/CountdownTimer";
-import { getMetaData, getViewports } from "@/lib/metadata";
-import Image from "next/image";
+import Image from 'next/image';
+import { Button, CountdownTimer } from '@/components';
+import { getMetaData, getViewports } from '@/lib';
 
 export const generateMetadata = () => getMetaData({});
-
 export const generateViewport = () => getViewports();
 
 export default function Home() {
   return (
-    <main className="container  flex flex-col mx-auto my-auto">
-      <div className="pt-8 flex flex-col items-center">
+    <main className="container mx-auto my-auto flex flex-col">
+      <div className="flex flex-col items-center pt-8">
         <h1 className="hidden">Spark Code</h1>
-        <h2 className="text-clamp-xl font-kufam font-bold mb-8 text-white">
-          Próximamente
-        </h2>
+        <h2 className="font-kufam mb-8 text-clamp-xl font-bold text-white">Próximamente</h2>
         <div className="max-w-[200px] sm:max-w-full">
-          <Image
-            src={"/images/spark-only.png"}
-            width={200}
-            height={100}
-            alt="Logo Spark Codes"
-          />
+          <Image src={'/images/spark-only.png'} width={200} height={100} alt="Logo Spark Codes" />
         </div>
         <CountdownTimer />
-        <h2 className="text-lg font-kufam font-bold mb-8 mt-8 text-white mt-2">
+        <h2 className="font-kufam my-8 text-lg font-bold text-white">
           Regístrate para los eventos:
         </h2>
-        <Button/>
+        <Button />
       </div>
     </main>
   );
