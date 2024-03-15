f720d73c2039e1aef6e095858915c494e7b06c13
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-15 14:08:55 -0300

    feat(navigation): ✨ implement responsive Navbar component
    Introduce the `Navbar` component within the `Navigation` directory, providing
    a responsive and accessible navigation solution for the application. Utilizing
    `useState` for state management, the component supports dynamic visibility of
    menu items based on the viewport size. The implementation leverages the `cn`
    utility for conditional class management, ensuring optimal styling across
    different screen sizes.
    
    - The `Navbar` component dynamically toggles between a hamburger icon and an
      X mark icon based on its open state, enhancing user interaction.
    - Implements accessible navigation links, ensuring users can navigate through
      the site's main areas with ease.
    - Utilizes the `Icon` component for rendering menu icons, maintaining consistency
      and reusability of UI elements.
    - Incorporates responsive design principles, making the navigation fully
      functional and visually appealing on both mobile and desktop viewports.
    
    This addition significantly improves the site's navigational structure, offering
    a seamless and user-friendly experience across all devices.
    
    fix(navigation): 💄 align navigation menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from './Button/Button';
 export * from './CountdownTimer/CountdownTimer';
 export * from './NumberBox/NumberBox';
 export * from './Footer/Footer';
+export * from './Navigation';
+export * from './Icon/Icon';
+export * from './Icon/IconCatalog';
