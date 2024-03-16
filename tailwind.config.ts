2ce64dfa910483fb90eebffe672a786c15cdd629
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-16 17:52:41 -0300

    refactor(tailwind): ♻️ update Tailwind CSS configuration for enhanced flexibility
    Revise the Tailwind CSS configuration to offer a more comprehensive and flexible
    styling solution. This update broadens the `content` configuration to include all
    files within the `src` directory, ensuring Tailwind's utility classes are
    accessible project-wide. Furthermore, the introduction of custom font families,
    `kufam` and `inter`, directly into the theme extends the typography options,
    aligning with the project's visual identity.
    
    Significant enhancements include:
    - Broadening the `content` path to `./src/**/*.{js,ts,jsx,tsx,mdx}`, ensuring
      Tailwind classes are universally available, facilitating a more integrated
      and cohesive design system across the project.
    - Integration of `kufam` and `inter` font families into the `fontFamily` section
      of the theme, promoting typography consistency and enriching text styling
      capabilities.
    - Expansion of the `colors` configuration into a scale for `primary` and
      `secondary` colors, providing a nuanced palette that supports a wide range of
      design needs and enhances the visual hierarchy.
    - Introduction of `fluid` font sizes leveraging CSS custom properties from the
      `_root.scss` file, offering responsive typography that adapts seamlessly to
      varying screen sizes, improving readability and user experience.
    
    These modifications not only refine the aesthetic flexibility but also bolster
    the adaptability and consistency of the design system within the project.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,64 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        kufam: ['Kufam', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
       backgroundImage: {
-        "star-pattern": "url('/images/estrellas-fondo.svg')",
+        'star-pattern': "url('/images/estrellas-fondo.svg')",
       },
       colors: {
-        "dark-purple": "#272039",
-        "light-purple": "#a586fc",
-        primary: "#6e0584",
-        secondary: "#35103c",
+        primary: {
+          50: 'hsl(250, 100%, 98%)',
+          100: 'hsl(251, 91%, 95%)',
+          200: 'hsl(251, 100%, 92%)',
+          300: 'hsl(253, 97%, 85%)',
+          400: 'hsl(256, 95%, 76%)',
+          500: 'hsl(259, 93%, 66%)',
+          600: 'hsl(263, 86%, 58%)',
+          700: 'hsl(264, 72%, 50%)',
+          800: 'hsl(264, 72%, 42%)',
+          900: 'hsl(264, 70%, 35%)',
+          950: 'hsl(262, 76%, 23%)',
+        },
+        secondary: {
+          50: 'hsl(235, 65%, 97%)',
+          100: 'hsl(240, 61%, 94%)',
+          200: 'hsl(240, 58%, 89%)',
+          300: 'hsl(244, 55%, 82%)',
+          400: 'hsl(247, 53%, 74%)',
+          500: 'hsl(252, 49%, 67%)',
+          600: 'hsl(256, 45%, 59%)',
+          700: 'hsl(258, 35%, 51%)',
+          800: 'hsl(257, 33%, 41%)',
+          900: 'hsl(256, 28%, 34%)',
+          950: 'hsl(257, 28%, 17%)',
+        },
       },
       fontSize: {
-        "clamp-xl": "clamp(2.25rem, 8vw, 3.75rem)",
+        'fluid-xs': 'var(--fluid-text-xs)',
+        'fluid-sm': 'var(--fluid-text-sm)',
+        'fluid-base': 'var(--fluid-text-base)',
+        'fluid-lg': 'var(--fluid-text-lg)',
+        'fluid-xl': 'var(--fluid-text-xl)',
+        'fluid-2xl': 'var(--fluid-text-2xl)',
+        'fluid-3xl': 'var(--fluid-text-3xl)',
+        'fluid-4xl': 'var(--fluid-text-4xl)',
+        'fluid-5xl': 'var(--fluid-text-5xl)',
+        'fluid-6xl': 'var(--fluid-text-6xl)',
+        'fluid-7xl': 'var(--fluid-text-7xl)',
       },
       animation: {
-        flip: "flip 1s cubic-bezier(0, 0, 0.2, 1) infinite",
+        flip: 'flip 1s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
       keyframes: {
         flip: {
-          from: { transform: "rotateX(0deg)", transformOrigin: "50% bottom " },
-          to: { transform: "rotateX(180deg)", transformOrigin: "50% bottom " },
+          from: { transform: 'rotateX(0deg)', transformOrigin: '50% bottom ' },
+          to: { transform: 'rotateX(180deg)', transformOrigin: '50% bottom ' },
         },
       },
     },
