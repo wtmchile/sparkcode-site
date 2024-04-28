b50592569970eecc0e67b56a499e03217f7864c9
Alfonsina <alfonsina.l@gmail.com>
2024-04-28 19:31:31 -0400

    fix: update pnpm lock file

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,28 +1,21 @@
-"use client";
-import { useState } from "react";
-import { Button } from "@/components/Button/Button";
-import { Title } from "@/components/agenda/Title";
-import { Timeline } from "@/components/Agenda/Timeline";
-import talksData from "@/talks/talks.json";
-import { AnimatePresence, motion } from "framer-motion";
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { Timeline } from '@/components/Agenda/Timeline';
+import { Title } from '@/components/Agenda/Title';
+import talksData from '@/talks/talks.json';
 
 export default function Agenda() {
   return (
-    <div className="flex flex-col w-full relative justify-center items-center">
-      <section className="lg:px-64 py-10 my-10 ">
-        <Title/>
+    <div className="relative flex w-full flex-col items-center justify-center">
+      <section className="my-10 py-10 lg:px-64 ">
+        <Title />
       </section>
- 
+
       <AnimatePresence>
-        
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Timeline data={talksData} />
-          </motion.div>
-        
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+          <Timeline data={talksData} />
+        </motion.div>
       </AnimatePresence>
     </div>
   );
