f87bd388ce1e0144ff76a895a70cedf9afd853f6
Daniela Cortez <d.cortezv@duocuc.cl>
2024-04-29 17:32:01 -0400

    closes #33

--- src/components/Agenda/Timeline.jsx
+++ src/components/Agenda/Timeline.jsx
@@ -20,12 +20,12 @@ const Card = ({
         e.target.scrollIntoView({ behavior: "smooth", block: "center" });
       }}
       className={cn(
-        "flex items-center w-[90vw] lg:w-[400px] min-h-[100px] h-fit relative flex flex-col mx-4 p-5 gap-5 cursor-pointer rounded-lg border border-purple-300 self-start bg-gradient-to-br from-[#0A0310] via-[#160E41] to-[#272039]",
+        "w-[90vw] lg:w-[420px] min-h-[160px]  h-fit relative flex flex-col mx-4 p-5 gap-5 border border-purple-300 bg-gradient-to-br from-[#0A0310] via-[#160E41] to-[#272039] rounded-lg shadow-xl cursor-pointer self-start",
         {
           "md:self-end": leftSide,
           "md:self-start": !leftSide,
           "opacity-30": status === "past",
-          "bg-[purple] text-white": status === "current",  
+          "bg-[purple] text-white": status === "current",
           "md:text-right": leftSide,
         },
         {
@@ -35,35 +35,37 @@ const Card = ({
       initial={{
         opacity: 0.5,
         scale: 0.8,
-        y: leftSide ? -10 : 10,
+        y: leftSide ? -20 : 20,
       }}
       margin="100px"
       whileInView={{
         opacity: 1,
         scale: 1,
-        y: -60,
+        y: -40,
       }}
     >
-      <div 
-        className={cn("flex flex-row gap-4 items-center opacity-50 hover:opacity-100", {
+      <div
+        className={cn("flex flex-row gap-4 opacity-50 hover:opacity-100", {
           "md:flex-row": leftSide,
         })}
       >
-        <span className="p-[2px] flex items-center justify-center w-12 h-12 bg-blue-100 rounded-full -start-6">
-          <Image
-            className="rounded-full aspect-square w-full h-full"
-            src={item.image}
-            alt={item.name}
-            // fill
-            width="48"
-            height="48"
-          />
-        </span>
-        <div className={"flex flex-col justify-center"}>
-        <time className="text-sm text-white leading-none font-barlow">
-        {startTime} - {endTime}
-      </time>
-          <span className="text-lg font-bold text-[#A586FC]">{"Speaker"}</span>
+        {item.image && (
+          <span className="flex-col rounded-full aspect-square w-20 h-20">
+            <Image
+              className="rounded-full aspect-square w-full h-full"
+              src={item.image}
+              alt={item.name}
+              // fill
+              width="48"
+              height="48"
+            />
+          </span>)}
+          <div className={"flex flex-col justify-center flex-grow"}>
+          <time className="text-sm text-white leading-none font-barlow">
+            {startTime} - {endTime}
+          </time>
+          <span className="text-lg font-bold text-[#A586FC]">{item.type}</span>
+          <span className="text-md font-bold text-white">{item.name}</span>
           {item.community !== "ninguna" && (
             <p className="text-sm font-bold text-white-400">
               <a href={item.communityUrl} target="_blank">
@@ -71,14 +73,12 @@ const Card = ({
               </a>
             </p>
           )}
-           <p className="text-base font-normal italic">
-        {'"'}
-        {item.topic}
-        {'"'}
-      </p>
+          <p className="text-base font-normal">
+            {item.topic}
+          </p>
         </div>
       </div>
-     
+
     </motion.li>
   );
 };
